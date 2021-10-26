0d7879d8c552c1fa0e016c0771d6248d1f15e340
Aline09 <oliveira.alineod@gmail.com>
2021-10-26 14:54:07 -0300

    feat: export album object

---
+++ data/albums.ts
@@ -0,0 +1,256 @@
+import { Album } from '../models/types';
+
+export const albums: Album[] = [
+  {
+    "id": "610006633f3c2caf80219c69",
+    "name": "Dangerous",
+    "artist": "Michael Jackson",
+    "created_at": "2003-10-05T08:42:25"
+  },
+  {
+    "id": "61000663e8d91dafb13dbc91",
+    "name": "Thriller",
+    "artist": "Michael Jackson",
+    "created_at": "1998-01-24T08:06:46"
+  },
+  {
+    "id": "610006638e311394e6abd194",
+    "name": "Off The Wall",
+    "artist": "Michael Jackson",
+    "created_at": "2015-11-17T12:28:50"
+  },
+  {
+    "id": "61000663bbdcaf0884991225",
+    "name": "Bad",
+    "artist": "Michael Jackson",
+    "created_at": "1999-04-27T05:50:50"
+  },
+  {
+    "id": "610006638aa5ebbf3482172c",
+    "name": "Forever Michael",
+    "artist": "Michael Jackson",
+    "created_at": "2009-09-06T06:23:40"
+  },
+  {
+    "id": "610006630b63d0f7b21a9390",
+    "name": "sit culpa ea",
+    "artist": "Marianne Day",
+    "created_at": "2014-04-20T02:38:18"
+  },
+  {
+    "id": "61000663acae0679e7fc37ac",
+    "name": "esse exercitation nulla",
+    "artist": "Phelps Dillard",
+    "created_at": "1992-09-30T03:40:00"
+  },
+  {
+    "id": "6100066346bd96554304b24d",
+    "name": "anim dolore quis",
+    "artist": "Castaneda Morris",
+    "created_at": "2018-08-20T10:30:51"
+  },
+  {
+    "id": "61000663a70933f9d30138e3",
+    "name": "ullamco id pariatur",
+    "artist": "Reva Wright",
+    "created_at": "2017-11-11T11:32:44"
+  },
+  {
+    "id": "61000663398febe90deb1413",
+    "name": "laborum incididunt voluptate",
+    "artist": "Joyner Baxter",
+    "created_at": "2017-01-23T03:44:21"
+  },
+  {
+    "id": "610006637de6be1da3c5f67e",
+    "name": "do do nostrud",
+    "artist": "Marquez Savage",
+    "created_at": "2010-08-15T11:32:54"
+  },
+  {
+    "id": "610006633b64c976baa3ba84",
+    "name": "elit velit eiusmod",
+    "artist": "Olivia Pacheco",
+    "created_at": "1983-06-13T11:47:04"
+  },
+  {
+    "id": "61000663cdf604651ec23cf7",
+    "name": "voluptate anim officia",
+    "artist": "Farmer Emerson",
+    "created_at": "1971-03-31T02:12:43"
+  },
+  {
+    "id": "61000663d8fbdff97f45eec1",
+    "name": "incididunt esse incididunt",
+    "artist": "Keri Rasmussen",
+    "created_at": "1970-05-15T11:24:55"
+  },
+  {
+    "id": "61000663cb8d4fcbe516506b",
+    "name": "et commodo duis",
+    "artist": "Araceli Henderson",
+    "created_at": "2001-03-26T10:03:28"
+  },
+  {
+    "id": "610006637c57a7a8f69406f1",
+    "name": "sit non ullamco",
+    "artist": "Hyde Wells",
+    "created_at": "2020-07-06T09:27:28"
+  },
+  {
+    "id": "6100066335cbe62a6c63d290",
+    "name": "laboris occaecat enim",
+    "artist": "Barrera Foreman",
+    "created_at": "1994-11-23T05:45:16"
+  },
+  {
+    "id": "61000663a1877138e13e74f1",
+    "name": "voluptate anim magna",
+    "artist": "Flowers Park",
+    "created_at": "1992-01-26T11:49:58"
+  },
+  {
+    "id": "610006635636c5ab6d96ded0",
+    "name": "ad nulla irure",
+    "artist": "Luella Ratliff",
+    "created_at": "2018-07-05T08:54:14"
+  },
+  {
+    "id": "6100066330d221233c256655",
+    "name": "sit incididunt non",
+    "artist": "Alford Boyer",
+    "created_at": "2012-10-17T09:02:20"
+  },
+  {
+    "id": "61000663113cfab2db87ca62",
+    "name": "exercitation qui est",
+    "artist": "Newton Berry",
+    "created_at": "2012-04-29T10:43:26"
+  },
+  {
+    "id": "61000663da8e7936d7d7c6a7",
+    "name": "eu dolor id",
+    "artist": "Reese Crawford",
+    "created_at": "2010-11-08T07:05:45"
+  },
+  {
+    "id": "610006637d753b7c16c31e53",
+    "name": "incididunt laboris reprehenderit",
+    "artist": "Beard Wise",
+    "created_at": "2001-02-06T01:05:24"
+  },
+  {
+    "id": "61000663849d9639194a0852",
+    "name": "proident velit exercitation",
+    "artist": "Louisa Ryan",
+    "created_at": "1993-06-01T04:09:52"
+  },
+  {
+    "id": "61000663b7cbe714001bcb4e",
+    "name": "reprehenderit cillum ea",
+    "artist": "Jeanie Gross",
+    "created_at": "2020-09-16T07:43:58"
+  },
+  {
+    "id": "6100066385843c8725a484c3",
+    "name": "esse laboris culpa",
+    "artist": "Candy Dotson",
+    "created_at": "2002-09-03T10:36:41"
+  },
+  {
+    "id": "61000663db98253f9b5dbe76",
+    "name": "anim commodo quis",
+    "artist": "Dawson Horn",
+    "created_at": "2002-12-22T06:10:57"
+  },
+  {
+    "id": "61000663d6cbe7e9f7689051",
+    "name": "sunt magna aute",
+    "artist": "Paul Howell",
+    "created_at": "2001-06-02T02:01:23"
+  },
+  {
+    "id": "610006633120ab8e6068f079",
+    "name": "in reprehenderit aliqua",
+    "artist": "Becker Franklin",
+    "created_at": "1988-05-12T12:29:29"
+  },
+  {
+    "id": "6100066363e21cceed258fee",
+    "name": "dolore magna aute",
+    "artist": "Huffman Clark",
+    "created_at": "1983-05-30T12:41:59"
+  },
+  {
+    "id": "61000663fb5ea8199a2946ed",
+    "name": "cillum nulla velit",
+    "artist": "Betsy Carroll",
+    "created_at": "1991-07-21T01:27:21"
+  },
+  {
+    "id": "6100066364dbf1911cba6fdf",
+    "name": "anim commodo pariatur",
+    "artist": "Allison Walker",
+    "created_at": "1981-12-29T02:08:40"
+  },
+  {
+    "id": "6100066350d5e2f61a27fb9a",
+    "name": "excepteur minim aliqua",
+    "artist": "Tran Cote",
+    "created_at": "2002-10-29T07:37:27"
+  },
+  {
+    "id": "61000663e20c2e3803237d30",
+    "name": "aliquip tempor do",
+    "artist": "Alison Levine",
+    "created_at": "2021-02-09T02:24:19"
+  },
+  {
+    "id": "610006631e8eddaecb7824c6",
+    "name": "enim proident tempor",
+    "artist": "Deidre Stevenson",
+    "created_at": "1973-03-15T04:10:28"
+  },
+  {
+    "id": "610006634629bf6a48199ba4",
+    "name": "culpa non proident",
+    "artist": "Compton Holloway",
+    "created_at": "1972-12-11T02:31:17"
+  },
+  {
+    "id": "61000663fc9a545c88283731",
+    "name": "labore deserunt Lorem",
+    "artist": "Padilla Cantrell",
+    "created_at": "1994-05-30T06:35:03"
+  },
+  {
+    "id": "610006630c6eda22e8c2dcf1",
+    "name": "occaecat consectetur reprehenderit",
+    "artist": "Ray Kaufman",
+    "created_at": "1986-10-26T07:37:29"
+  },
+  {
+    "id": "6100066353bb491877567161",
+    "name": "minim laborum labore",
+    "artist": "Madeline Buchanan",
+    "created_at": "1984-10-05T10:34:07"
+  },
+  {
+    "id": "61000663803e47cd8ff27436",
+    "name": "The Hammer",
+    "artist": "Michael Witt",
+    "created_at": "1988-12-14T05:10:36"
+  },
+  {
+    "id": "61000663508b42ab47f8e519",
+    "name": "quis laboris tempor",
+    "artist": "Shawna Campos",
+    "created_at": "2019-04-05T08:09:55"
+  },
+  {
+    "id": "6100066390ac23059d26f5bf",
+    "name": "aliqua nulla non",
+    "artist": "Johanna Stevens",
+    "created_at": "1983-09-15T01:44:34"
+  }
+]
