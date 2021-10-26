805cfe15e9911cf27d84ac3efc014cc0ab0e8c5d
Aline09 <oliveira.alineod@gmail.com>
2021-10-26 16:27:42 -0300

    feat: add soundtracks data object

---
+++ data/soundtracks.ts
@@ -0,0 +1,261 @@
+import { Soundtrack } from "../models/types"
+export const soundtracks : Soundtrack[]  = [
+    {
+      "id": "61000902c541fb45580e3b55",
+      "album_id": "610006633f3c2caf80219c69",
+      "name": "commodo nisi magna",
+      "minutes": 7
+    },
+    {
+      "id": "610009023ce086056b919bcc",
+      "album_id": "61000663e8d91dafb13dbc91",
+      "name": "et dolor aliqua",
+      "minutes": 5
+    },
+    {
+      "id": "61000902c4037e9bd8cd2cf4",
+      "album_id": "610006638e311394e6abd194",
+      "name": "do do in",
+      "minutes": 8
+    },
+    {
+      "id": "610009028b5a191d0fcfe803",
+      "album_id": "61000663bbdcaf0884991225",
+      "name": "ut mollit elit",
+      "minutes": 1
+    },
+    {
+      "id": "610009028b5a191d0fcfe803",
+      "album_id": "61000663bbdcaf0884991225",
+      "name": "ut mollit elitt",
+      "minutes": 1
+    },
+    {
+      "id": "610009027cda3310ad703a65",
+      "album_id": "610006638aa5ebbf3482172c",
+      "name": "officia esse aliquip",
+      "minutes": 3
+    },
+    {
+      "id": "61000902c832b8442a896e66",
+      "album_id": "610006630b63d0f7b21a9390",
+      "name": "ipsum aliquip enim",
+      "minutes": 10
+    },
+    {
+      "id": "61000902982da186742d8aee",
+      "album_id": "61000663acae0679e7fc37ac",
+      "name": "est do duis",
+      "minutes": 8
+    },
+    {
+      "id": "61000902f346c258a5abb074",
+      "album_id": "6100066346bd96554304b24d",
+      "name": "Lorem exercitation et",
+      "minutes": 10
+    },
+    {
+      "id": "61000902292a8f1768281bae",
+      "album_id": "61000663a70933f9d30138e3",
+      "name": "ut nisi voluptate",
+      "minutes": 4
+    },
+    {
+      "id": "61000902f8c23c242be24883",
+      "album_id": "61000663398febe90deb1413",
+      "name": "laborum consectetur elit",
+      "minutes": 8
+    },
+    {
+      "id": "610009028a031ffe9fe165f7",
+      "album_id": "610006637de6be1da3c5f67e",
+      "name": "qui exercitation esse",
+      "minutes": 6
+    },
+    {
+      "id": "61000902fc4d82429fee7d08",
+      "album_id": "610006633b64c976baa3ba84",
+      "name": "non aliquip amet",
+      "minutes": 4
+    },
+    {
+      "id": "6100090252a63ae7e2a1b144",
+      "album_id": "61000663cdf604651ec23cf7",
+      "name": "ipsum sint aliqua",
+      "minutes": 6
+    },
+    {
+      "id": "610009029760fc113691bd04",
+      "album_id": "61000663d8fbdff97f45eec1",
+      "name": "enim proident quis",
+      "minutes": 4
+    },
+    {
+      "id": "61000902e6d3119573b1b0b0",
+      "album_id": "61000663cb8d4fcbe516506b",
+      "name": "duis exercitation fugiat",
+      "minutes": 3
+    },
+    {
+      "id": "610009021cd7f0b4293369fe",
+      "album_id": "610006637c57a7a8f69406f1",
+      "name": "labore in consectetur",
+      "minutes": 7
+    },
+    {
+      "id": "6100090258e48c184dd1561f",
+      "album_id": "6100066335cbe62a6c63d290",
+      "name": "reprehenderit excepteur mollit",
+      "minutes": 9
+    },
+    {
+      "id": "61000902fa0b9a42dffa141c",
+      "album_id": "61000663a1877138e13e74f1",
+      "name": "aute ad consequat",
+      "minutes": 7
+    },
+    {
+      "id": "610009028b34bc4d1df642ff",
+      "album_id": "610006635636c5ab6d96ded0",
+      "name": "ea labore mollit",
+      "minutes": 3
+    },
+    {
+      "id": "61000902c6e47a7199b69e84",
+      "album_id": "6100066330d221233c256655",
+      "name": "proident excepteur excepteur",
+      "minutes": 6
+    },
+    {
+      "id": "61000902a5a64ea116bed11e",
+      "album_id": "61000663113cfab2db87ca62",
+      "name": "elit enim sunt",
+      "minutes": 7
+    },
+    {
+      "id": "61000902c6377b200df6a688",
+      "album_id": "61000663da8e7936d7d7c6a7",
+      "name": "ad ut excepteur",
+      "minutes": 9
+    },
+    {
+      "id": "610009021392fef3e6b46699",
+      "album_id": "610006637d753b7c16c31e53",
+      "name": "occaecat voluptate laborum",
+      "minutes": 6
+    },
+    {
+      "id": "61000902f3c32c2d1d599694",
+      "album_id": "61000663849d9639194a0852",
+      "name": "amet ipsum excepteur",
+      "minutes": 7
+    },
+    {
+      "id": "61000902c0f0142ffa0e27bc",
+      "album_id": "61000663b7cbe714001bcb4e",
+      "name": "ex excepteur elit",
+      "minutes": 10
+    },
+    {
+      "id": "610009020da0dadd146cb5a1",
+      "album_id": "6100066385843c8725a484c3",
+      "name": "magna incididunt pariatur",
+      "minutes": 3
+    },
+    {
+      "id": "61000902039296be17cc76a4",
+      "album_id": "61000663db98253f9b5dbe76",
+      "name": "fugiat tempor duis",
+      "minutes": 9
+    },
+    {
+      "id": "61000902e78b66540cf38bf1",
+      "album_id": "61000663d6cbe7e9f7689051",
+      "name": "officia ex enim",
+      "minutes": 6
+    },
+    {
+      "id": "6100090286cd423699b3a546",
+      "album_id": "610006633120ab8e6068f079",
+      "name": "excepteur tempor reprehenderit",
+      "minutes": 10
+    },
+    {
+      "id": "6100090263a1577056fd566b",
+      "album_id": "6100066363e21cceed258fee",
+      "name": "culpa et esse",
+      "minutes": 3
+    },
+    {
+      "id": "610009029ebdc5d4cb64dd46",
+      "album_id": "61000663fb5ea8199a2946ed",
+      "name": "in ea non",
+      "minutes": 4
+    },
+    {
+      "id": "6100090294c0b5f4f74e2cd9",
+      "album_id": "6100066364dbf1911cba6fdf",
+      "name": "fugiat ut qui",
+      "minutes": 10
+    },
+    {
+      "id": "61000902ccfa8db6f2f1171b",
+      "album_id": "6100066350d5e2f61a27fb9a",
+      "name": "excepteur fugiat Lorem",
+      "minutes": 5
+    },
+    {
+      "id": "61000902792ca7c7f0f47e02",
+      "album_id": "61000663e20c2e3803237d30",
+      "name": "officia adipisicing deserunt",
+      "minutes": 4
+    },
+    {
+      "id": "610009026dcb99ee8f2307ad",
+      "album_id": "610006631e8eddaecb7824c6",
+      "name": "tempor ex aliquip",
+      "minutes": 3
+    },
+    {
+      "id": "61000902fef80f4434b63ace",
+      "album_id": "610006634629bf6a48199ba4",
+      "name": "minim proident aute",
+      "minutes": 10
+    },
+    {
+      "id": "61000902985d3a5501bb1b55",
+      "album_id": "61000663fc9a545c88283731",
+      "name": "excepteur magna sint",
+      "minutes": 4
+    },
+    {
+      "id": "61000902c668a5259f6c037b",
+      "album_id": "610006630c6eda22e8c2dcf1",
+      "name": "qui cillum tempor",
+      "minutes": 4
+    },
+    {
+      "id": "610009020b07730d9a79d5fa",
+      "album_id": "6100066353bb491877567161",
+      "name": "nostrud occaecat elit",
+      "minutes": 3
+    },
+    {
+      "id": "610009021bda1bd039577384",
+      "album_id": "61000663803e47cd8ff27436",
+      "name": "consectetur voluptate reprehenderit",
+      "minutes": 3
+    },
+    {
+      "id": "610009027d8b4977b203b606",
+      "album_id": "61000663508b42ab47f8e519",
+      "name": "ullamco quis laboris",
+      "minutes": 8
+    },
+    {
+      "id": "61000902ed9d95448a48a19d",
+      "album_id": "6100066390ac23059d26f5bf",
+      "name": "dolore consectetur amet",
+      "minutes": 8
+    }
+  ]
