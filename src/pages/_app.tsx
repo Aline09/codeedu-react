c16d0a4d2a156bb085bb0f160c71ed77da5c50c6
Aline09 <oliveira.alineod@gmail.com>
2021-10-26 21:08:31 -0300

    feat: configure application store provider and theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useEffect } from "react";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "../store";
+import Theme from "../theme";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles)
+    }
+  }, [])
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Provider store={store}>
+        <Theme>
+          <Component {...pageProps} />
+        </Theme>
+      </Provider>
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
